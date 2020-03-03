2d66433ed3a5984af786e984307906563bcf5f10
LexxxusPtitsa <55672271+LexxxusPtitsa@users.noreply.github.com>
2020-03-04 01:34:14 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,50 @@
 let programmNavLink = document.querySelectorAll('.programm__nav-li');
 console.log(programmNavLink);
 
-[].forEach.call(programmNavLink, function(item) {
-    item.addEventListener('click', function(e) {
+[].forEach.call(programmNavLink, function (item) {
+    item.addEventListener('click', function (e) {
         e.preventDefault();
         // console.log(e.target.tagName);
-        if(e.target.tagName === 'A')
-        document.querySelector('.programm__nav-link--active').classList.remove('programm__nav-link--active');
+        if (e.target.tagName === 'A')
+            document.querySelector('.programm__nav-link--active').classList.remove('programm__nav-link--active');
         document.querySelector('.programm__nav-li--active').classList.remove('programm__nav-li--active');
         document.querySelector('.programm__description-inner--active').classList.remove('programm__description-inner--active');
         item.classList.add('programm__nav-li--active');
         let id = e.target.href.split('#')[1];
-        document.querySelector('#'+id).classList.add('programm__description-inner--active');
+        document.querySelector('#' + id).classList.add('programm__description-inner--active');
         e.target.classList.add('programm__nav-link--active');
     });
-  });
+});
+
+
+$(document).ready(function () {
+    $('.slider_first').slick({
+        infinite: true,
+        dots: true,
+        customPaging: function (slider, i) {
+            console.log(slider,i)
+            return '<span class="dot"></span>';
+        },
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $('.slider_second').slick({
+        infinite: true,
+        // dots: true,
+        customPaging: function (slider, i) {
+            console.log(slider,i)
+            return '<span class="dot"></span>';
+        },
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
